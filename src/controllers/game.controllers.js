aa0664c4fb75e8d8119ce272c83aba07de07d0b7
GDMeira <gabrieldega970@gmail.com>
2023-07-25 11:44:50 -0300

    feature: criado o endpoint de get lista de jogos.

--- src/controllers/game.controllers.js
+++ src/controllers/game.controllers.js
@@ -4,3 +4,7 @@ export async function postGame(req, res) {
     //TODO: salvar jogo no banco
     res.status(201).send('Jogo adicionado ao banco.');
 }
+
+export async function getGames(req, res) {
+    res.send('lista de jogos.');
+}
--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { schemaValidation } from "../middlewares/schemaValidation.js";
 import { gameSchema } from "../schemas/game.schemas.js";
-import { postGame } from "../controllers/game.controllers.js";
+import { getGames, postGame } from "../controllers/game.controllers.js";
 
 
 const gameRouter = Router();
 
 gameRouter.post('/games', schemaValidation(gameSchema), postGame);
+gameRouter.get('/games', getGames);
 
 export default gameRouter
