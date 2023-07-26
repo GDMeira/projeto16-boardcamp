ad67a996e17746c66ef768ce690ffae36b5297fd
GDMeira <gabrieldega970@gmail.com>
2023-07-25 23:12:30 -0300

    refac: endpoint de post game agora consegue se conectar com o postgres corretamente.

--- src/controllers/game.controllers.js
+++ src/controllers/game.controllers.js
@@ -1,8 +1,23 @@
+import { db } from "../database/database.connection.js";
 
 
 export async function postGame(req, res) {
-    //TODO: salvar jogo no banco
-    res.status(201).send('Jogo adicionado ao banco.');
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
+    try {
+        const search = await db.query(`SELECT * FROM games WHERE name = $1`, [name]);
+        if (search.rowCount) return res.sendStatus(409);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
+    try {
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay")
+            VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 }
 
 export async function getGames(req, res) {
