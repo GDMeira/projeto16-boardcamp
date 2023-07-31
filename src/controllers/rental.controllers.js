045eeca5a128017cddec0ae241919b5447e04f76
GDMeira <gabrieldega970@gmail.com>
2023-07-31 00:30:40 -0300

    feat: implementado o endpoint de delete rental.

--- src/controllers/rental.controllers.js
+++ src/controllers/rental.controllers.js
@@ -75,3 +75,24 @@ export async function returnRental(req, res) {
         res.status(500).send(error.message);
     }
 }
+
+export async function deleteRental(req, res) {
+    const { id } = req.params;
+
+    try {
+        const search = await db.query(`
+            SELECT * 
+            FROM rentals
+            WHERE id = $1;`, [id]);
+        
+        if (!search.rowCount) return res.sendStatus(404);
+        if (!search.rows[0].returnDate) return res.sendStatus(400);
+
+        await db.query(`
+            DELETE FROM rentals WHERE id = $1;`, [id]);
+
+        res.sendStatus(200);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
--- src/routes/rental.routes.js
+++ src/routes/rental.routes.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { rentalSchema } from "../schemas/rent.schemas.js";
 import { stringStripHtml } from "../middlewares/stringStripHtmlValidation.js";
 import { schemaValidation } from "../middlewares/schemaValidation.js";
-import { getRentals, postRental, returnRental } from "../controllers/rental.controllers.js";
+import { deleteRental, getRentals, postRental, returnRental } from "../controllers/rental.controllers.js";
 
 const rentalRouter = Router();
 
 rentalRouter.post('/rentals', stringStripHtml, schemaValidation(rentalSchema), postRental);
 rentalRouter.get('/rentals', getRentals);
 rentalRouter.get('/rentals/:id/return', returnRental);
-// rentalRouter.put('/customers/:id', stringStripHtml, schemaValidation(rentalSchema), updateCustomer);
+rentalRouter.delete('/rentals/:id', deleteRental);
 
 export default rentalRouter
